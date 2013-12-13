2e0c7d6bae2b62bd00eaa258f66c65f06cdca1b3
Mathieu Lajugie <mathieu@caring.com>
2013-12-12 18:26:01 -0800

    cleanup test

--- test/asset_pipeline_test.exs
+++ test/asset_pipeline_test.exs
@@ -4,8 +4,6 @@ defmodule AssetPipelineTest do
   alias DynamoCompanion.AssetPipeline
 
   test "request static asset content in dev mode" do
-    AssetPipeline.start_link
     assert AssetPipeline.render_bundle("javascripts/allo.js") == %s{"ze content";}
-    AssetPipeline.stop
   end
 end
--- test/fixtures/assets/javascripts/application.js
+++ test/fixtures/assets/javascripts/application.js
@@ -1,3 +1,4 @@
+//= require foundation/foundation.alerts
 //= require ./included
 
 window.APP = {};
