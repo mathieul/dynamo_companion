3d2a39cba13bbc0f40bf9fde54a5ad4eea890915
Mathieu Lajugie <mathieu@caring.com>
2013-12-12 18:19:57 -0800

    fix first test

--- test/asset_pipeline_test.exs
+++ test/asset_pipeline_test.exs
@@ -5,7 +5,7 @@ defmodule AssetPipelineTest do
 
   test "request static asset content in dev mode" do
     AssetPipeline.start_link
-    assert AssetPipeline.get(:dev, "allo.js") == "ze content"
+    assert AssetPipeline.render_bundle("javascripts/allo.js") == %s{"ze content";}
     AssetPipeline.stop
   end
 end
